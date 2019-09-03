3c7fe0ccaf9e41dfda5abc544ebdf5709e3fd22c
lapeev16@gmail.com <lapeev16@gmail.com>
2019-09-03 21:57:54 +0300

    Updates

---
+++ precache-manifest.4affc4bcf469874357a75a1ddfd09b1e.js
@@ -0,0 +1,218 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d40f7357c6a2785d3990ec362b6b4a6f",
+    "url": "/infox/index.html"
+  },
+  {
+    "revision": "a891b4d1e28b04ad1a12",
+    "url": "/infox/static/css/main.788710a5.chunk.css"
+  },
+  {
+    "revision": "661dcd8d930dc6732788",
+    "url": "/infox/static/js/2.cef1c08e.chunk.js"
+  },
+  {
+    "revision": "a891b4d1e28b04ad1a12",
+    "url": "/infox/static/js/main.55a830b5.chunk.js"
+  },
+  {
+    "revision": "7d1345525dae109dfd9b",
+    "url": "/infox/static/js/runtime~main.0d890961.js"
+  },
+  {
+    "revision": "95ee688c142b5746c7634ee60a9262b9",
+    "url": "/infox/static/media/24-hours-support.95ee688c.svg"
+  },
+  {
+    "revision": "a5f6a8af8ee0e38bbabf6899b93a4b11",
+    "url": "/infox/static/media/arrow-right-copy.a5f6a8af.svg"
+  },
+  {
+    "revision": "54278d200faeb03c5bdb38bfab2aab1a",
+    "url": "/infox/static/media/arrow-right.54278d20.svg"
+  },
+  {
+    "revision": "688ad1fd6375850810e72d943271e399",
+    "url": "/infox/static/media/background-bubles-light.688ad1fd.webp"
+  },
+  {
+    "revision": "738e12ba7a0909bd75247dc4d38d4ab4",
+    "url": "/infox/static/media/background-bubles-light.738e12ba.jpg"
+  },
+  {
+    "revision": "beafc065c3fe884b039444be8ac18049",
+    "url": "/infox/static/media/background-bubles-light.beafc065.jp2"
+  },
+  {
+    "revision": "4aa7a30d7c438a2ad30446a780b7a736",
+    "url": "/infox/static/media/background-bubles.4aa7a30d.png"
+  },
+  {
+    "revision": "e4cfcb7ced090b94c12b6b4271e7244f",
+    "url": "/infox/static/media/background-bubles.e4cfcb7c.webp"
+  },
+  {
+    "revision": "e53e456d475d4afb1f6f2a0d7ab37f86",
+    "url": "/infox/static/media/background-bubles.e53e456d.jp2"
+  },
+  {
+    "revision": "03ad29fd9b39b08fb51d83f2c2b05029",
+    "url": "/infox/static/media/e-mail.03ad29fd.svg"
+  },
+  {
+    "revision": "81ca3a964ac0ee71edace130b320c34b",
+    "url": "/infox/static/media/facebook.81ca3a96.svg"
+  },
+  {
+    "revision": "70eefe969a41e8e3f5d4bbdbaff41a1e",
+    "url": "/infox/static/media/kyivstar.70eefe96.svg"
+  },
+  {
+    "revision": "4f569d0e3b4dfc7095d679b9d97fc041",
+    "url": "/infox/static/media/life.4f569d0e.svg"
+  },
+  {
+    "revision": "5ddf8f7bd9788c1234192ec8e40ae78e",
+    "url": "/infox/static/media/lk-citizenry.5ddf8f7b.svg"
+  },
+  {
+    "revision": "803ee90be0f5993b7b3d4b4e0f2f6d08",
+    "url": "/infox/static/media/lk-enterprise.803ee90b.svg"
+  },
+  {
+    "revision": "a3d20697b80a3ca74ce1896e732681a4",
+    "url": "/infox/static/media/lk-press.a3d20697.svg"
+  },
+  {
+    "revision": "9ac0eff4b9ae3b5baf824a0819b454be",
+    "url": "/infox/static/media/logo_mob.9ac0eff4.jp2"
+  },
+  {
+    "revision": "acb9758e4accd75aeed74b6506f336d1",
+    "url": "/infox/static/media/logo_mob.acb9758e.webp"
+  },
+  {
+    "revision": "3ee0a4777f1ec95efa76d8969e528a63",
+    "url": "/infox/static/media/logo_ru.3ee0a477.png"
+  },
+  {
+    "revision": "74cfe330f5c2722179dfa2ed4b794523",
+    "url": "/infox/static/media/logo_ru.74cfe330.webp"
+  },
+  {
+    "revision": "d8cc2ba7c4d2af1e7216ef0e022a0cd9",
+    "url": "/infox/static/media/logo_ru.d8cc2ba7.jp2"
+  },
+  {
+    "revision": "1b6284b9535e44d2a2b40f28e4fd2518",
+    "url": "/infox/static/media/logo_uk.1b6284b9.jp2"
+  },
+  {
+    "revision": "5eb866f0c77d5ccc6ef27e39a0eb29bb",
+    "url": "/infox/static/media/logo_uk.5eb866f0.webp"
+  },
+  {
+    "revision": "beedfd3545d0b2ef9d840f8841e67c7d",
+    "url": "/infox/static/media/logo_uk.beedfd35.png"
+  },
+  {
+    "revision": "115a176042fb1c026be009e965cb1b3b",
+    "url": "/infox/static/media/officeBackgrund.115a1760.jp2"
+  },
+  {
+    "revision": "aa13f86cf97e4ad6490cc6980dd78108",
+    "url": "/infox/static/media/officeBackgrund.aa13f86c.jpg"
+  },
+  {
+    "revision": "f2e78318123c6d28781764e2dfc2eb0f",
+    "url": "/infox/static/media/officeBackgrund.f2e78318.webp"
+  },
+  {
+    "revision": "100a24fc1e4f86e0577f45249ab4bcba",
+    "url": "/infox/static/media/password.100a24fc.svg"
+  },
+  {
+    "revision": "37b1050e41436b612a24609935b5c0f9",
+    "url": "/infox/static/media/quickMenuBackground-first.37b1050e.jpg"
+  },
+  {
+    "revision": "5dd94c78fe074851c88bf0969c61e811",
+    "url": "/infox/static/media/quickMenuBackground-first.5dd94c78.jp2"
+  },
+  {
+    "revision": "7f020a0be85f64795a5543516c8baed5",
+    "url": "/infox/static/media/quickMenuBackground-first.7f020a0b.webp"
+  },
+  {
+    "revision": "6d01e7b4bce53b2b458a51ee7a4f7a60",
+    "url": "/infox/static/media/quickMenuBackground-firstIcon.6d01e7b4.svg"
+  },
+  {
+    "revision": "064c6d75b972055d2603bde4662e43c9",
+    "url": "/infox/static/media/quickMenuBackground-fourth.064c6d75.jp2"
+  },
+  {
+    "revision": "2ad411c509c19bd76794697461c94e68",
+    "url": "/infox/static/media/quickMenuBackground-fourth.2ad411c5.webp"
+  },
+  {
+    "revision": "340459aeb1dd4da295f17992c3cb9487",
+    "url": "/infox/static/media/quickMenuBackground-fourth.340459ae.jpg"
+  },
+  {
+    "revision": "c92096f75be04970fd74eb4551e7ded8",
+    "url": "/infox/static/media/quickMenuBackground-fourthIcon.c92096f7.svg"
+  },
+  {
+    "revision": "38a8dc902c0ffeaf0e0afbdf35416942",
+    "url": "/infox/static/media/quickMenuBackground-second.38a8dc90.jp2"
+  },
+  {
+    "revision": "b231b99d7a702aa2d2002c0d217b7007",
+    "url": "/infox/static/media/quickMenuBackground-second.b231b99d.jpg"
+  },
+  {
+    "revision": "f43f074de37dc043b1d0ddb7375c8fc9",
+    "url": "/infox/static/media/quickMenuBackground-second.f43f074d.webp"
+  },
+  {
+    "revision": "a84ccf3031e3591fdd4961d21a36af4c",
+    "url": "/infox/static/media/quickMenuBackground-secondIcon.a84ccf30.svg"
+  },
+  {
+    "revision": "6513771312f3e1b1c596a54aa923a9db",
+    "url": "/infox/static/media/quickMenuBackground-third.65137713.jpg"
+  },
+  {
+    "revision": "ad01aa4d35f528139393b58e129593d8",
+    "url": "/infox/static/media/quickMenuBackground-third.ad01aa4d.webp"
+  },
+  {
+    "revision": "db660ee3784dbe895861afb9b5769b38",
+    "url": "/infox/static/media/quickMenuBackground-third.db660ee3.jp2"
+  },
+  {
+    "revision": "58fd4eade9a1068674d5364146d63243",
+    "url": "/infox/static/media/quickMenuBackground-thirdIcon.58fd4ead.svg"
+  },
+  {
+    "revision": "0289a9127031eb1523b6bf0c8d934b69",
+    "url": "/infox/static/media/tel.0289a912.svg"
+  },
+  {
+    "revision": "ccd38cb8a5bc4f0feafcd1a73ee9422f",
+    "url": "/infox/static/media/telegram.ccd38cb8.svg"
+  },
+  {
+    "revision": "598bd32e32ffed42cf2d5921dd739b39",
+    "url": "/infox/static/media/user-gray-office-back.598bd32e.svg"
+  },
+  {
+    "revision": "8564e39ab70226c581876379d3d4a51f",
+    "url": "/infox/static/media/viber.8564e39a.svg"
+  },
+  {
+    "revision": "33d1f90e5dbd17271e19a7bfa9f71f0b",
+    "url": "/infox/static/media/vodafone.33d1f90e.svg"
+  }
+]);
